7c5d4eeaeda191376bd9c03f1346dd849630d689
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-09-04 15:10:04 +0330

    complete code
    this code for accounting its false i want in next commit delete accounts app and again write new api code for new accounts appp

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import register, user_login, profile, logout
-
+from accounts.api.views import UserRegistrationAPIView, UserDetailAPIView
 
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/',user_login,name='login'),
     path('profile/',profile,name='profile'),
     path('logout',logout,name='logout'),
+
+    path('api/register/',UserRegistrationAPIView.as_view(),name='api-register'),
+    path('api/user/<int:pk>/',UserDetailAPIView.as_view(),name='api-user-detail'),
 ]
