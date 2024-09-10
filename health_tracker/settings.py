3099d7b3d30a800d5e4d39019a845b4efc41a016
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-09-10 13:53:08 +0330

    swagger
    create swagger urls and install drf yasg

--- health_tracker/settings.py
+++ health_tracker/settings.py
@@ -31,16 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'drf_yasg',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    
     'django.contrib.staticfiles',
     'accounts',
     # 'rest_framework.authtoken',
     'tracker',
+
+    
 ]
 
 MIDDLEWARE = [
